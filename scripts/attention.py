96d09146464e6e51ae605889624c93be21a1c4f8
KVS Mohan Vamsi <konudroid@gmail.com>
2024-05-29 14:56:35 +0530

    understood and created the multi head attention function

---
+++ scripts/attention.py
@@ -0,0 +1,55 @@
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def scaled_dot_product(q, k, v, mask=None):
+    d_k = q.size()[-1]
+    scaled = torch.matmul(q, k.transpose(-1, -2)) / math.sqrt(d_k)
+    if mask is not None:
+        scaled += mask
+    attention = F.softmax(scaled, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
+
+class MultiheadAttention(nn.Module):
+
+    def __init__(self, input_dim, d_model, num_heads):
+        super().__init__()
+        self.input_dim = input_dim
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+        self.qkv_layer = nn.Linear(input_dim , 3 * d_model)
+        self.linear_layer = nn.Linear(d_model, d_model)
+    
+    def forward(self, x, mask=None):
+        batch_size, sequence_length, input_dim = x.size()
+        print(f"x.size(): {x.size()}")
+        qkv = self.qkv_layer(x)
+        print(f"qkv.size(): {qkv.size()}")
+        qkv = qkv.reshape(batch_size, sequence_length, self.num_heads, 3 * self.head_dim)
+        print(f"qkv.size(): {qkv.size()}")
+        qkv = qkv.permute(0, 2, 1, 3)
+        print(f"qkv.size(): {qkv.size()}")
+        q, k, v = qkv.chunk(3, dim=-1)
+        print(f"q size: {q.size()}, k size: {k.size()}, v size: {v.size()}, ")
+        values, attention = scaled_dot_product(q, k, v, mask)
+        print(f"values.size(): {values.size()}, attention.size:{ attention.size()} ")
+        values = values.reshape(batch_size, sequence_length, self.num_heads * self.head_dim)
+        print(f"values.size(): {values.size()}")
+        out = self.linear_layer(values)
+        print(f"out.size(): {out.size()}")
+        return out
+    
+input_dim = 1024
+d_model = 512
+num_heads = 8
+
+batch_size = 30
+sequence_length = 5
+x = torch.randn( (batch_size, sequence_length, input_dim) )
+
+model = MultiheadAttention(input_dim, d_model, num_heads)
+out = model.forward(x)
